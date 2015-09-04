fb054dd84cf4a5bdcc9ede2009c65a9795b9313f
Jason Seney <jasonseney@tumblr.com>
2015-09-03 21:32:27 -0400

    Fix for calculating max size

--- AutoSizingTextField/AutoSizingTextView.swift
+++ AutoSizingTextField/AutoSizingTextView.swift
@@ -9,16 +9,38 @@
 import Foundation
 import UIKit
 
-protocol AutoSizingTextViewDelegate {
-    func didChangeHeight(height: CGFloat)
+/**
+*  A delegate to handle specific functionality related to the resizing of a text view.
+*/
+protocol AutoSizingTextViewDelegate : UITextViewDelegate {
+    /**
+    Handle a change in an auto sizing text view's height
+    
+    :param: changeInHeight The number of points the height changed. (Positive if growing, negative if shrinking)
+    */
+    func didChangeHeight(changeInHeight: CGFloat)
 }
 
 class AutoSizingTextView: UITextView, UITextViewDelegate {
     
-    let sizingDelegate: AutoSizingTextViewDelegate
-    let maxHeight: CGFloat
+    // An optional delegate used to handle specific resizing actions
+    private weak var sizingDelegate: AutoSizingTextViewDelegate?
     
-    init(maxHeight: CGFloat, sizingDelegate: AutoSizingTextViewDelegate) {
+    // The maximum height value to grow to, after which the view starts to scroll
+    private let maxHeight: CGFloat
+    
+    // Sensible default for convience so the textview doesn't grow out of control
+    static private let defaultMaxHeight = CGFloat(100)
+    
+    /**
+    Creates a new instance of an auto sizing text view
+    
+    :param: maxHeight      The max height to grow to, afterwhich the text view scrolls. Defaults to 100.
+    :param: sizingDelegate An optional delegate to
+    
+    :returns: A new instance of an auto sizing text view
+    */
+    init(maxHeight: CGFloat = AutoSizingTextView.defaultMaxHeight, sizingDelegate: AutoSizingTextViewDelegate?) {
         self.sizingDelegate = sizingDelegate
         self.maxHeight = maxHeight
         
@@ -27,35 +49,58 @@ class AutoSizingTextView: UITextView, UITextViewDelegate {
         self.delegate = self
         self.scrollEnabled = false
     }
-
+    
     required init(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-    func calcSize() -> CGSize {
-        return self.sizeThatFits(CGSize(width: self.bounds.width, height: CGFloat.max))
+    /**
+    Calculates the size that fits all the text based on the
+    current width of this view.
+    
+    :returns: The width and height that can fit all the text
+    */
+    private func calcSize() -> CGSize {
+        let fittedSize = sizeThatFits(CGSize(width: self.bounds.width, height: maxHeight))
+        
+        /**
+        * Make sure that the fitted size is ACTUALLY shorter than our max height
+        * Surprisingly, sizeThatFits CAN RETURN a size that is taller than the height of the size passed in.
+        */
+        return fittedSize.height <= maxHeight ? fittedSize : CGSize(width:self.bounds.width, height: maxHeight)
     }
+    
+    // MARK - UIView
     
     override func intrinsicContentSize() -> CGSize {
         return calcSize()
     }
     
+    // MARK - UITextViewDelegate
+    
+    func textView(textView: UITextView, shouldChangeTextInRange range: NSRange, replacementText text: String) -> Bool {
+        return sizingDelegate?.textView?(textView, shouldChangeTextInRange: range, replacementText: text) ?? true
+    }
+    
     func textViewDidChange(textView: UITextView) {
         
-        let newSize = calcSize()
-        
-        if newSize.height > self.maxHeight {
-            self.scrollEnabled = true
-        } else {
-            self.scrollEnabled = false
-        }
-        
+        var newHeight = calcSize().height
         let oldHeight = self.frame.height
         
-        self.updateConstraints()
+        // When we've maxed out, turn scrolling on
+        if newHeight >= maxHeight {
+            scrollEnabled = true
+        }
+        else {
+            scrollEnabled = false
+        }
         
-        if oldHeight != newSize.height {
-            sizingDelegate.didChangeHeight(newSize.height)
+        updateConstraints()
+        
+        let changeInHeight = newHeight - oldHeight
+        if changeInHeight != 0 {
+            sizingDelegate?.didChangeHeight(changeInHeight)
         }
     }
+
 }
